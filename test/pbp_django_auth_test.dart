ddfcf1677a5433f254b7bc0010bc2fc7b5316777
Muhammad Athallah <mhd.athallah@gmail.com>
2022-11-10 20:37:15 +0700

    remove: tests

--- test/pbp_django_auth_test.dart
+++ test/pbp_django_auth_test.dart
@@ -1,12 +1,1 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:pbp_django_auth/pbp_django_auth.dart';
-
-void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-  });
-}
+// TODO: Add test?
